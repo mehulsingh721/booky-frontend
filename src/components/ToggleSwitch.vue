061c4db84f556e292c5c45fb81efa41710196264
Mehul Singh Rathore <mehulsingh072001@gmail.com>
2022-07-04 07:55:47 +0530

    solo event topbar complete

---
+++ src/components/ToggleSwitch.vue
@@ -0,0 +1,58 @@
+<script>
+export default {
+  name: "ToggleSwitch",
+  props: {
+    enabled: Boolean,
+  },
+};
+</script>
+
+<template>
+  <div class="toggle-container" :class="{enabled: enabled, disabled: !enabled}">
+    <p v-if="enabled">ON</p>
+    <div class="dialog-button"></div>
+    <p v-if="!enabled">OFF</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.toggle-container {
+  width: 7rem;
+  height: 3.5rem;
+  border-radius: 6px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  cursor: pointer;
+  padding: 0 .5rem;
+}
+
+.dialog-button {
+  background-color: white;
+  height: 2rem;
+  width: 2rem;
+  border-radius: 50%;
+}
+p{
+  font-size: 1.4rem;
+}
+
+.enabled {
+  border: 1px solid #038164;
+  p{
+    color: #038164;
+    padding-left: 1rem;
+  }
+  .dialog-button {
+    background-color: #038164;
+  }
+}
+
+.disabled {
+  background-color: #757575;
+  p{
+    color: white;
+    padding-right: .5rem;
+  }
+}
+</style>
--- src/views/Home/SoloEventView.vue
+++ src/views/Home/SoloEventView.vue
@@ -1,11 +1,72 @@
 <script>
-// code...
+import ToggleSwitch from "../../components/ToggleSwitch.vue"
+import { ref } from "vue"
+export default {
+  name: "SoloEventView",
+  components: {
+    ToggleSwitch
+  },
+  setup(){
+    const eventStatus = ref(false);
+    const toggleEventStatus = () => {
+      eventStatus.value = !eventStatus.value
+    }
+    return { eventStatus, toggleEventStatus }
+  }
+}
 </script>
 
 <template>
-  <h1>Hello World</h1>
+  <div class="solo-bar">
+    <div class="container">
+      <div class="wrapper">
+        <button class="btn-sec">Back</button>
+        <h1>Create New Event Type</h1>
+        <div class="switch" @click="toggleEventStatus">
+          <p>Your event type is</p>
+          <ToggleSwitch :enabled="eventStatus" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="solo-content">
+    <div class="row-1"></div>
+    <div class="row-2"></div>
+    <div class="row-3"></div>
+  </div>
 </template>
 
-<style scoped>
-/* code... */
+<style scoped lang="scss">
+.solo-bar {
+  background-color: var(--color-body);
+  padding-top: 2rem;
+  box-shadow: 0 20px 20px -20px rgba(0, 0, 0, .15);
+
+  .container {
+    padding: 2rem 2rem;
+    max-width: 55%;
+    margin: auto;
+    width: 100%;
+    margin-bottom: 4rem;
+
+    .wrapper {
+      display: flex;
+      max-width: 100%;
+      justify-content: space-between;
+      align-items: center;
+
+      h1 {
+        font-size: 2rem;
+        font-weight: 400;
+        color: var(--color-text);
+      }
+      .switch{
+        display: flex;
+        align-items: center;
+        column-gap: 1rem;
+        font-size: 1.6rem;
+      }
+    }
+  }
+}
 </style>e
